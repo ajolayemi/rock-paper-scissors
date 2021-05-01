3561010ad4b8ff13ba92e8f59bef13aad1d63782
jolayemi_local <ajolayemi82@gmail.com>
2021-05-01 19:36:08 +0200

    Completed stage 2

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+import random
 
 RULES = {'rock': 'scissors',
          'paper': 'rock',
          'scissors': 'paper'}
 
-if __name__ == '__main__':
+
+def game_player():
     user_weapon = input()
-    computer_weapon = list(filter(lambda x: RULES[x] == user_weapon, RULES.keys()))[0]
-    print(f'Sorry, but the computer chose {computer_weapon}')
+    random_computer_weapon = random.choice(list(RULES.keys()))
+    if user_weapon == random_computer_weapon:
+        print(f'There is a draw ({user_weapon})')
+    elif RULES.get(user_weapon) == random_computer_weapon:
+        print(f'Well done. The computer chose {random_computer_weapon} and failed')
+    else:
+        print(f'Sorry, but the computer chose {random_computer_weapon}')
+
+
+if __name__ == '__main__':
+    game_player()
