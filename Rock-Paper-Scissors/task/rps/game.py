5a78bb0a9bc4cd67cb997715c06dd133a395b264
jolayemi_local <ajolayemi82@gmail.com>
2021-05-01 19:28:43 +0200

    Completed first stage!

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+
+RULES = {'rock': 'scissors',
+         'paper': 'rock',
+         'scissors': 'paper'}
+
+if __name__ == '__main__':
+    user_weapon = input()
+    computer_weapon = list(filter(lambda x: RULES[x] == user_weapon, RULES.keys()))[0]
+    print(f'Sorry, but the computer chose {computer_weapon}')
