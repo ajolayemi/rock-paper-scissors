8e368fe77282300399788f78effb5302ab76eb4f
jolayemi_local <ajolayemi82@gmail.com>
2021-05-01 19:40:24 +0200

    Completed stage 3

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -7,14 +7,21 @@ RULES = {'rock': 'scissors',
 
 
 def game_player():
-    user_weapon = input()
-    random_computer_weapon = random.choice(list(RULES.keys()))
-    if user_weapon == random_computer_weapon:
-        print(f'There is a draw ({user_weapon})')
-    elif RULES.get(user_weapon) == random_computer_weapon:
-        print(f'Well done. The computer chose {random_computer_weapon} and failed')
-    else:
-        print(f'Sorry, but the computer chose {random_computer_weapon}')
+    while True:
+        user_weapon = input()
+        random_computer_weapon = random.choice(list(RULES.keys()))
+        if user_weapon == '!exit':
+            print('Bye')
+            break
+        if user_weapon not in RULES.keys():
+            print('Invalid input')
+
+        elif user_weapon == random_computer_weapon:
+            print(f'There is a draw ({user_weapon})')
+        elif RULES.get(user_weapon) == random_computer_weapon:
+            print(f'Well done. The computer chose {random_computer_weapon} and failed')
+        else:
+            print(f'Sorry, but the computer chose {random_computer_weapon}')
 
 
 if __name__ == '__main__':
